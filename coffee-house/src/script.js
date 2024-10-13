fcad19afad10a9e50b4b763b5ad3a0f9d83c3884
Anna <yaroshenkoannette@gmail.com>
2024-10-13 15:12:55 +0200

    feat: add burger-menu

--- coffee-house/src/script.js
+++ coffee-house/src/script.js
@@ -1,4 +1,6 @@
-import {switchMenu} from "../modules/switch.js"
+import {switchMenu} from "../modules/switch.js";
+
+
 
 const switches = document.querySelector(".section__switches");
     switches.addEventListener("click", switchMenu);
